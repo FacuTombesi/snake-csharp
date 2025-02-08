6984a653bff71594401962d92fd18d6de25c7b9a
FacuTombesi <facundomtombesi@gmail.com>
2025-02-08 17:35:39 -0300

    update(+): food class

--- Food.cs
+++ Food.cs
@@ -4,5 +4,10 @@ public class Food
 {
   private Random _random = new Random();
 
+  public (int x, int y) Position { get; private set; }
 
+  public void GenerateNewPosition(int width, int height)
+  {
+    Position = (_random.Next(1, width - 1), _random.Next(1, height - 1));
+  }
 }
